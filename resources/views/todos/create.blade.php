88d8cfb2993f161a8e9d38cb9976cebed5c90508
roseeen <rneilrosen@gmail.com>
2024-04-16 19:07:14 +0800

    minor

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -5,7 +5,19 @@
     <div class="row">
         <div class="col-md-6">
             <h1>Create Todo</h1>
-            <!-- Add your code here for the form to create a new todo -->
+            <form action="{{ route('todos.store') }}" method="POST">
+    @csrf
+    <div class="form-group">
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title" class="form-control" required>
+    </div>
+    <div class="form-group">
+        <label for="description">Description</label>
+        <textarea name="description" id="description" class="form-control"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary">Create</button>
+</form>
+
         </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
 use App\Http\Controllers\TodoController;
 
 Route::middleware(['auth'])->group(function () {
